5200bba5b08b0ef4b53fb063edab8ba3c644eaca
patelamancode <74897715+patelamancode@users.noreply.github.com>
2021-01-17 23:03:30 +0530

    completing components without functioning

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-
-    </div>
-  );
+// this particleObject is comming from react-partical.js it shows background effect.
+const particalObject = {
+  particles: {
+    number: {
+      value:150,
+      density: {
+        enable: true,
+        value_area:750
+      }
+    }
+  }
+}
+class App extends React.Component {
+  render(){  
+    return (
+      <div className="App">
+          <Particles className='particles'
+            params={particalObject}
+         />
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm/>
+      </div>
+     );
+  }  
 }
 
 export default App;
