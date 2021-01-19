6da9ec7de3a1e80d60fd229521145bfc01a7c571
patelamancode <74897715+patelamancode@users.noreply.github.com>
2021-01-19 21:29:19 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
+import FaceRecognition from './components/facerecognition/FaceRecognition';
 import Rank from './components/rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
 import './App.css';
 
 
-// this particleObject is comming from react-partical.js it shows background effect.
+// this particleObject is comming from "react-partical.js" it shows background effect.
+
+
+const app = new Clarifai.App({
+  apiKey: 'fdd1ada30f534811a38953615eff72a2'
+});
+
+
+
+// @@
+// const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+// const stub = ClarifaiStub.grpc();
+
+// // This will be used by every Clarifai endpoint call.
+// const metadata = new grpc.Metadata();
+// metadata.set("authorization","fdd1ada30f534811a38953615eff72a2");
+// @@@
+
+
+
 const particalObject = {
   particles: {
     number: {
@@ -18,8 +40,45 @@ const particalObject = {
       }
     }
   }
+  // onhover: {
+  //   enable: true,
+  //   mode: 'repulse'
+  // }
 }
+
+
+
 class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state= {
+      input: '',
+    }
+  }
+
+  // events happen on imagelinkform in input,button tag.
+  onInputChange = (event) =>{  
+    console.log(event.target.value);
+  }
+
+  onDetectBtn = () => {
+    console.log('click');
+
+
+    app.models
+     .predict("eeed0b6733a644cea07cf4c60f87ebb7",  "https://samples.clarifai.com/face-det.jpg")
+       .then(
+         function(response) {
+          console.log(response)
+         },
+         function(err) {
+
+         }
+        )
+  }
+
+
   render(){  
     return (
       <div className="App">
@@ -29,10 +88,15 @@ class App extends React.Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm 
+          onInputChange = {this.onInputChange}
+          onDetectBtn = {this.onDetectBtn}
+        />
+        <FaceRecognition/>
       </div>
-     );
-  }  
-}
+       );
+   }  
+}  
+
 
 export default App;
--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onDetectBtn}) => {
     return(
         <div>
             <p className= 'f3'>
@@ -10,8 +10,11 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br2 shadow-5'>
-                  <input className= 'f4 pa2 w-70 center' placeholder='drag your url here...' type='text'/>
-                  <button className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple '>Detect</button>
+                  <input className= 'f4 pa2 w-70 center' placeholder='drag your url here...' type='text' onChange = {onInputChange}/>
+                  <button 
+                  className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple '
+                  onClick = {onDetectBtn}
+                  >Detect </button>
                 </div>
             </div>
         </div>
